34bc5e61748932e4bb5c5522d56fbd7b26c92d2c
Jeff <jefferson.vivas.95@gmail.com>
2025-09-10 17:15:53 +0200

    update: instalacion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import { 'tailwindcss' } from .Config;} */
+
+module.exports = {
+    content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
